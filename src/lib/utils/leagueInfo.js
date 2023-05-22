47377a0ddee09955770c44ca943c6beae93df5be
skateandlacroix <91497938+skateandlacroix@users.noreply.github.com>
2023-05-22 10:34:24 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -26,8 +26,7 @@ export const managers = [
 //    "roster": 1,  // (DEPRECATED! Don't use this anymore) ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID": "198866830238945280",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
       "name": "Mike",
-      "tookOver": "", // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
-      "location": "", // (optional)
+      "location": "C-Dot", // (optional)
       "bio": "",
       "photo": "/managers/mikel.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
